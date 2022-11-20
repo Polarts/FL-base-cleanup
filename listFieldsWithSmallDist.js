43e7d5e25ffbb60991ae532526a5e1c158e6210e
Polarts <polarbear.oc@gmail.com>
2022-11-20 10:18:32 +0200

    Added fields with small dist

---
+++ listFieldsWithSmallDist.js
@@ -0,0 +1,29 @@
+const { getCommonArgs, getIniFiles, getSectionsFromIni, printProgress, padNum } = require("./functions");
+
+const { shouldExport } = getCommonArgs();
+
+async function getFields() {
+    const asteroidFields = await getIniFiles("../DATA/SOLAR/ASTEROIDS");
+    console.log(`Got ${asteroidFields.length} asteroid field files.`);
+    return asteroidFields;
+}
+
+async function listFieldsWithSmallDist() {
+    const fieldFiles = await getFields();
+    const smolFields = [];
+    for ([file, fileIdx] of fieldFiles.entries()) {
+        printProgress(`Checking field ${padNum(fileIdx+1, 3)}/${fieldFiles.length} for fill_dist <= 1400...`);
+        const fieldSection = (await getSectionsFromIni(file, "Field"))[0];
+        if (fieldSection.fill_dist <= 1400) {
+            smolFields.push(file);
+        }
+    }
+    console.log();
+    console.log(smolFields);
+    if (shouldExport) {
+        console.log("Exporting result to file ./output/unusedFields.txt");
+        await writeListToFile("fieldsWithSmallDist", smolFields);
+    }
+}
+
+listFieldsWithSmallDist();
