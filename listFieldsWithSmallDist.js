8beb436aa983c0609bf300ec35a4e4a1b5c3f0c2
Polarts <polarbear.oc@gmail.com>
2022-11-20 11:33:55 +0200

    Added dist arg

--- listFieldsWithSmallDist.js
+++ listFieldsWithSmallDist.js
@@ -2,6 +2,9 @@ const { getCommonArgs, getIniFiles, getSectionsFromIni, printProgress, padNum } 
 
 const { shouldExport } = getCommonArgs();
 
+const argIdx = process.argv.indexOf("--dist");
+const fillDist = argIdx[argIdx+1] ?? 1600;
+
 async function getFields() {
     const asteroidFields = await getIniFiles("../DATA/SOLAR/ASTEROIDS");
     console.log(`Got ${asteroidFields.length} asteroid field files.`);
@@ -14,7 +17,7 @@ async function listFieldsWithSmallDist() {
     for ([file, fileIdx] of fieldFiles.entries()) {
         printProgress(`Checking field ${padNum(fileIdx+1, 3)}/${fieldFiles.length} for fill_dist <= 1400...`);
         const fieldSection = (await getSectionsFromIni(file, "Field"))[0];
-        if (fieldSection.fill_dist <= 1400) {
+        if (fieldSection.fill_dist <= fillDist) {
             smolFields.push(file);
         }
     }
