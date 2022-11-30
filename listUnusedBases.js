5f8383024970e065dbe8c9d5752ae7e6f652a565
Polarts <davidklein.4496@gmail.com>
2022-11-30 21:57:58 +0200

    Fixed exclude not working well in listUnusedBases

--- listUnusedBases.js
+++ listUnusedBases.js
@@ -10,15 +10,17 @@ async function listUnusedBases() {
     const badBases = [];
     for ([baseIdx, base] of bases.entries()) {
         const nick = base.nickname;
-        let hasBase = false;
-        for ([systemIdx, file] of systemFiles.entries()) {
-            printProgress(`base ${padNum(baseIdx + 1, 3)}/${bases.length} - system ${padNum(systemIdx + 1, 3)}/${systemFiles.length} - file ${file}`);
-            const text = await fs.readFile(file, 'utf-8');
-            if (text.toLocaleLowerCase().includes(nick.toLocaleLowerCase()) 
-                && !excludeSystems.some(sys => file.toLocaleLowerCase().includes(sys.toLocaleLowerCase()))) {
-                hasBase = true;
+        let hasBase = excludeSystems.some(sys => sys.toLocaleLowerCase().includes(base.system.toLocaleLowerCase()));
+        if (!hasBase) {
+            for ([systemIdx, file] of systemFiles.entries()) {
+                printProgress(`base ${padNum(baseIdx + 1, 3)}/${bases.length} - system ${padNum(systemIdx + 1, 3)}/${systemFiles.length} - file ${file}`);
+                const text = await fs.readFile(file, 'utf-8');
+                if (text.toLocaleLowerCase().includes(nick.toLocaleLowerCase())) {
+                    hasBase = true;
+                }
             }
         }
+        // if it still doesn't have it
         if (!hasBase) {
             badBases.push(nick);
         }
